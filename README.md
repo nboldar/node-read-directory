4018188894786ac80cf5b0bf2c65c0eec15e16dd
Nikolay Boldar <nboldar1@gmail.com>
2020-02-29 10:22:55 +0300

    Function done

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # node-read-directory
-nodejs simple function for reading directory recursively, no matter how deep is it
+Nodejs simple lightweight function for reading directory recursively, no matter how deep is it
+##Instalation
+npm i node-read-directory
+
+##Usage
+const readDirectory = require('node-read-directory');
+
+readDirectory('./sourceDir').then( data => console.log(data)).catch( error => console.error(error));
+##How it works
+For example source directory looks like below:
+C:\USERS\USER\PROJECTS\UNTITLED\SRC
+│   1.png
+│
+├───emptyDir
+└───thumbs
+    │   50-1.png
+    │
+    └───newDir
+            30-2.png
+
+So you get this:
+ [
+  {
+    root: 'C:\\', \n
+    dir: 'C:\\Users\\User\\Projects\\untitled\\src',
+    base: '1.png',
+    ext: '.png',
+    name: '1',
+    readDir: 'C:\\Users\\User\\Projects\\untitled\\src'
+  },
+  {
+    root: 'C:\\',
+    dir: 'C:\\Users\\User\\Projects\\untitled\\src',
+    base: 'emptyDir',
+    ext: '',
+    name: 'emptyDir',
+    readDir: 'C:\\Users\\User\\Projects\\untitled\\src'
+  },
+  {
+    root: 'C:\\',
+    dir: 'C:\\Users\\User\\Projects\\untitled\\src\\thumbs',
+    base: '50-1.png',
+    ext: '.png',
+    name: '50-1',
+    readDir: 'C:\\Users\\User\\Projects\\untitled\\src'
+  },
+  {
+    root: 'C:\\',
+    dir: 'C:\\Users\\User\\Projects\\untitled\\src\\thumbs\\newDir',
+    base: '30-2.png',
+    ext: '.png',
+    name: '30-2',
+    readDir: 'C:\\Users\\User\\Projects\\untitled\\src'
+  }
+]
+
---
+++ index.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+/**
+ * Function read directory asynchronous, recursively going around all directories and files included
+ * @param {String} src - path to source directory
+ * @returns {Promise<[]>} Promise resolve array of objects with info about files and empty directories included
+ * in source directory, example:
+ *  [ {
+    root: 'C:\\',
+    dir: 'C:\\Users\\User\\Projects\\untitled\\src',
+    base: '1.png',
+    ext: '.png',
+    name: '1',
+    readDir: 'C:\\Users\\User\\Projects\\untitled\\src'
+  },
+ {
+    root: 'C:\\',
+    dir: 'C:\\Users\\User\\Projects\\untitled\\src\\thumbs\\newDir',
+    base: 'emptyDirectory',
+    ext: '',
+    name: 'emptyDirectory',
+    readDir: 'C:\\Users\\User\\Projects\\untitled\\src'
+  }]
+ */
+const readDirectory = async (src) => {
+    if (typeof src !== 'string') throw  new Error('Path of read dir should be string');
+    const arr = [];
+    src = path.isAbsolute(src) ? src : path.resolve(__dirname, src);
+    const rootDir = src;
+    const iterateDir = async (src) => {
+        try {
+            let result = await fs.promises.readdir(src, {encoding: 'utf8', withFileTypes: true});
+            if (result.length === 0) {
+                let data = path.parse(src);
+                data.readDir = rootDir;
+                arr.push(data)
+            }
+            for await (const dirent of result) {
+                if (dirent.isDirectory()) {
+                    let tempResult = await iterateDir(`${src + path.sep + dirent.name}`)
+                } else {
+                    let data = path.parse(`${src + path.sep + dirent.name}`);
+                    data.readDir = rootDir;
+                    arr.push(data)
+                }
+            }
+            return arr;
+        } catch (err) {
+            console.log(err);
+        }
+    };
+    return await iterateDir(src);
+};
+module.exports = readDirectory;
